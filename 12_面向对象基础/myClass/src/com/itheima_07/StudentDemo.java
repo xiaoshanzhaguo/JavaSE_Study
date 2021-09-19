e779046f63f4367f7255e9949d4c3c612993a658
xiaoshanzhaguo <xiaoshanzhaguo@163.com>
2021-09-19 22:33:12 +0800

    complete to 面向对象基础-标准类制作

---
+++ 12_面向对象基础/myClass/src/com/itheima_07/StudentDemo.java
@@ -0,0 +1,10 @@
+package com.itheima_07;
+
+public class StudentDemo {
+    public static void main(String[] args) {
+        //创建对象
+        Student s = new Student();
+
+        s.show();
+    }
+}
