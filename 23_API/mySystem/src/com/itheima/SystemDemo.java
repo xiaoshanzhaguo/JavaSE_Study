5076cbb3dc859a655f0bebd83a7163e82c29f5ca
xiaoshanzhaguo <xiaoshanzhaguo@163.com>
2021-10-06 17:26:56 +0800

    complete to the System

---
+++ 23_API/mySystem/src/com/itheima/SystemDemo.java
@@ -0,0 +1,29 @@
+package com.itheima;
+
+/*
+    System类的常用方法
+ */
+public class SystemDemo {
+    public static void main(String[] args) {
+        /*
+        System.out.println("开始");
+        //public static void exit(int status)：终止当前运行的 Java 虚拟机，非零表示异常终止
+        System.exit(0);
+        System.out.println("结束");
+        */
+
+        //public static long currentTimeMillis()：返回当前时间(以毫秒为单位)
+//        System.out.println(System.currentTimeMillis());
+
+        //下面是计算跟1970年相差多少的计算。
+        // *1.0是变成浮点数，/1000是毫秒的缘故，/60是分, /60是秒，/24是小时，/365是天
+//        System.out.println(System.currentTimeMillis() * 1.0 / 1000 / 60 / 60 / 24 / 365);
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            System.out.println(i);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("共耗时：" + (end - start) + "毫秒");
+    }
+}
